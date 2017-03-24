cffeabfaba472c6b044065aec42f4f345cc3ec9c
Chris Graffeo <cgraffeo1@gmail.com>
2017-03-24 16:12:51 -0700

    added base registering functionality

--- main.py
+++ main.py
@@ -14,6 +14,24 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+
+
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+
+
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
 
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -32,6 +50,7 @@ class BlogHandler(webapp2.RequestHandler):
     def render_post(response, posst):
         response.out.write('<b>' + post.subject + '</br><br>')
         response.out.write(post.content)
+
 
 class HomePage(BlogHandler):
     def get(self):
@@ -93,6 +112,55 @@ class NewPost(BlogHandler):
             error = "New posts must contain a subject and content!"
             self.render('newpost.html', subject=subject, content=content,
                         error=error)
+
+
+class Signup(BlogHandler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        have_error = False
+        self.username = self.request.get('username')
+        self.password = self.reqest.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
+
+        params = dict(username=self.username, email=self.email)
+
+        if not valid_username(self.username):
+            params['error_username'] = "That is not a valid username"
+            have_error = True
+
+        if not valid_password(self.password):
+            params['error_password'] = "That is not a valid password"
+        elif self.password != self.verify:
+            params['error_verify'] = "Your passwords did not match"
+
+        if not valid_email(self.email):
+            params['error_email'] = "That is not a valid Email"
+
+        if have_error:
+            self.render('signup.html', **params)
+        else:
+            self.done()
+
+        def done(self, *a, **kw):
+            raise NotImplementedError
+
+
+class Register(Signup):
+    def done(self):
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists'
+            self.render('signup.html', error_username=msg)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+
+            self.login(u)
+            self.redirect('/blog')
+
 
 app = webapp2.WSGIApplication([('/', HomePage)
                                ],
